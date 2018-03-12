d3b32915fc9c0189042636af7ad7460a167ddd94
Paulo Henrique <p.henriquerm@hotmail.com>
2018-03-11 23:49:20 -0300

    create imoveis project

---
+++ app/Imovel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Imovel extends Model
+{
+    protected $fillable = [
+    	'id',
+    	'descricao',
+    	'logradouroEndereco',
+    	'bairroEndereco',
+    	'numeroEndereco',
+    	'cepEndereco',
+    	'cidadeEndereco',
+    	'preco',
+    	'qtdQuartos',
+    	'tipo',
+    	'finalidade'
+    ];
+
+    protected $table = "imoveis";
+}
